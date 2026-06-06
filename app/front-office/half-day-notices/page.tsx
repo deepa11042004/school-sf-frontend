a3a79e536f0fda90adf29c1b03151ee4c2f10453
Rohit Chauhan <nixenboi@gmail.com>
2026-06-06 16:34:40 +0530

    front office

--- app/front-office/half-day-notices/page.tsx
+++ app/front-office/half-day-notices/page.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import Layout from "@/components/cmsfullform/layout"
-import HalfDayNotices from "@/components/front-office/halfdaynotices";
+import HalfDayNotices from "@/components/front-office/HalfDayNotices";
 export const metadata: Metadata = {
   title: " SF-Software Dashboard - OpenSource CMS",
   description: " SF-Software dashboard build with Next.js and Tailwind CSS",
